18eade6fa588c4350e32e10fe5dd8dcccf66a4df
Timmy <votung1597@gmail.com>
2021-08-29 16:13:14 +0700

    add examples/ex_file.txt

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+const fs = require("fs");
+
+console.log('start executing tasks...');
+fs.readFile("./examples/ex_file.txt", 'utf8', function(err, data){
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(data);
+    console.log('complete task');
+});
+
+console.log('continue executing other task...');
